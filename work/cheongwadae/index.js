17d277928812ae06c4ab6f60ab969caac13d18d2
ahyoung611 <wlq0611@naver.com>
2024-12-28 00:28:56 +0900

    work 작업물 1차 완성

--- work/cheongwadae/index.js
+++ work/cheongwadae/index.js
@@ -1,53 +1,39 @@
 
-const sections = document.querySelectorAll('section')
-const footer = document.querySelector('footer')
-const sectLast = sections[sections.length - 1]
-const sectLastTop = sectLast.offsetTop
+const sections = document.querySelectorAll('section');
+const footer = document.querySelector('footer');
+const sectLast = sections[sections.length - 1];
+const sectLastTop = sectLast.offsetTop;
 
-
-
-//whell control 제어
+// wheel control 제어
 sections.forEach((sect, i) => {
-    sect.addEventListener('wheel', function (e) {
-        e.preventDefault()
+    sect.addEventListener(
+        'wheel',
+        function (e) {
+            e.preventDefault();
 
-        //마우스 휠 내리면 100, 올리면 -100 표시
-        let delta = e.deltaY
+            let delta = e.deltaY;
 
-        //첫번째 섹션 위로 가지 않도록
-        if (delta < 0 && i === 0) {
-            return
+            // 첫 번째 섹션 위로 가지 않도록
+            if (delta < 0 && i === 0) {
+                return;
+            }
 
-            //마지막 섹션 아래로 가지 않도록   
-        } else if (delta > 0 && i === sections.length - 1) {
-            //footer 보이게: 하나 위치 찾아갈 때 scrollintoview 사용
-            footer.scrollIntoView({ behavior: 'smooth' })
-            return
-        }
+            // 마지막 섹션 아래로 가지 않도록
+            if (delta > 0 && i === sections.length - 1) {
+                footer.scrollIntoView({ behavior: 'smooth' });
+                return;
+            }
 
+            let secTop = delta < 0 ? sect.previousElementSibling : sect.nextElementSibling;
+            if (secTop) {
+                let targetSecTop = secTop.offsetTop;
+                window.scrollTo({ top: targetSecTop, behavior: 'smooth' });
+            }
+        },
+        { passive: false }
+    );
+});
 
-        let secTop = delta < 0 ? sect.previousElementSibling : sect.nextElementSibling
-        if (secTop) {
-            let targetSecTop = secTop.offsetTop
-            window.scrollTo({ top: targetSecTop, behavior: 'smooth' })
-        }
-    })
-})
-
-//전체 문서의 height 길이 페이지 전체가 3000
-// console.log(document.body.scrollHeight);
-
-// scrollY 값 = 2000까지 내렸어
-// window.height 값 1080
-// 2000 + 1080 = 스크롤의 top 위치 값
-
-window.addEventListener('wheel', function (e) {
-    //window.innerHeight + window.scrollY = 현재 스크롤 위치
-    if (e.deltaY < 0 && window.innerHeight + window.scrollY >= document.body.scrollHeight - 10) {
-        e.preventDefault()
-        window.scrollTo({ top: sectLastTop, behavior: 'smooth' })
-    }
-})
 
 //nav
 $('nav li.main-menu').mouseenter(function () {
@@ -109,7 +95,7 @@ gsap.registerEffect({
         const index = target[0].dataset.index; //인덱스 값 읽어들인다.
         const tl = gsap.timeline()
 
-        tl.from(chars, { x: x, opacity: 0, filter: 'blur(5px)', stagger: 0.1})
+        tl.from(chars, { x: x, opacity: 0, filter: 'blur(5px)', stagger: 0.1 })
             //bg 안에 첫번째 두번째
             .to(`.bg > div:nth-child(${index})`, { opacity: 1 }, 0)
             .to(chars, { delay: 3, opacity: 0, y: y, stagger: 0.05 })
@@ -133,7 +119,7 @@ function textAnimation() {
 
 textAnimation();
 
-
+//sect4 ----------------------------------------
 $('.mn-list li').mouseenter(function () {
     $(this).addClass('on')
 })
@@ -142,6 +128,7 @@ $('.mn-list li').mouseleave(function () {
     $(this).removeClass('on')
 })
 
+//sect5 ------------------------------------------
 $('.contents-list li').click(function (e) {
     e.preventDefault()
     let idx = $(this).index()
@@ -151,3 +138,111 @@ $('.contents-list li').click(function (e) {
     $('.contents-list a').removeClass('on')
     $(this).find('a').addClass('on')
 })
+
+document.addEventListener("DOMContentLoaded", () => {
+    var RSwiper = new Swiper(".R-Swiper", {
+        slidesPerView: 1,
+        spaceBetween: 0,
+
+        navigation: {
+            nextEl: "#review .swiper-button-next",
+            prevEl: "#review .swiper-button-prev",
+        },
+
+        breakpoints: {
+            642: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+            1190: {
+                slidesPerView: 4,
+                spaceBetween: 30,
+            },
+        },
+    });
+
+    var PSwiper = new Swiper(".P-Swiper", {
+        slidesPerView: 1,
+        grid: {
+            rows: 1,
+        },
+        spaceBetween: 0,
+
+        navigation: {
+            nextEl: "#photo .swiper-button-next",
+            prevEl: "#photo .swiper-button-prev",
+        },
+
+        breakpoints: {
+            641: {
+                slidesPerView: 2,
+                grid: {
+                    rows: 1,
+                },
+                spaceBetween: 20,
+            },
+            1190: {
+                slidesPerView: 4,
+                grid: {
+                    rows: 2,
+                },
+                spaceBetween: 30,
+            },
+        },
+    });
+    
+    var VSwiper = new Swiper(".V-Swiper", {
+        slidesPerView: 1,
+        grid: {
+            rows: 1,
+        },
+        spaceBetween: 0,
+
+        navigation: {
+            nextEl: "#video .swiper-button-next",
+            prevEl: "#video .swiper-button-prev",
+        },
+
+        breakpoints: {
+            641: {
+                slidesPerView: 2,
+                grid: {
+                    rows: 1,
+                },
+                spaceBetween: 20,
+            },
+            1190: {
+                slidesPerView: 4,
+                grid: {
+                    rows: 2,
+                },
+                spaceBetween: 30,
+            },
+        },
+    });
+
+    var NSwiper = new Swiper(".N-Swiper", {
+        slidesPerView: 1,
+        spaceBetween: 0,
+
+        navigation: {
+            nextEl: "#news .swiper-button-next",
+            prevEl: "#news .swiper-button-prev",
+        },
+
+        breakpoints: {
+            641: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+            1190: {
+                slidesPerView: 4,
+                spaceBetween: 30,
+            },
+        },
+    });
+
+});
+
+
+
